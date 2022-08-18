156e26f74a08b542bcad7621c05fbad8ec528254
John Rey Payos <99696836+JohnRey-P@users.noreply.github.com>
2022-08-18 08:56:33 +0800

    Add files via upload

---
+++ Payos_Perimeter_of_Rectangle.py
@@ -0,0 +1,13 @@
+class Rectangle:
+   def __init__(self,length,width):
+     self.length = length
+     self.width = width
+
+   def Perimeter(self):
+       print(2 * (self.length + self.width))
+
+
+rect = Rectangle(7,3)
+print( "Perimeter of the Rectangle:")
+rect.Perimeter()
+
